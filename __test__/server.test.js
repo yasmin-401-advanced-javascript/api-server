a8f0fe86b33f352cb4b3b5edcc0c20805cb72d6c
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-02 21:24:43 +0300

    400.js , 500.js test DONE

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -3,7 +3,7 @@ const { server } = require('../lib/server.js');
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 
-describe('server modul', () => {
+describe('server module', () => {
   describe('category', () => {
     it('should respond with 200 on valied route', () => {
       return mockRequest.get('/api/v1/categories').then((results) => {
