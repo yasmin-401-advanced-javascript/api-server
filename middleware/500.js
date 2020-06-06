c1509283ca2ed28249a01577d297c1c92caccbaf
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-07 00:00:08 +0300

    500.js adding next

--- middleware/500.js
+++ middleware/500.js
@@ -1,8 +1,9 @@
 'use strict';
 
-module.exports = (err,req, res) => {
+module.exports = (err,req, res, next) => {
     
   res.status(500);
   res.statusMessage = 'Server Error :(';
   res.json({ error: err });  
+  next();
 };
