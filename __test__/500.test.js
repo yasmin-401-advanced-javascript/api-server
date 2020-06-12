8509c7521774f78f9abc1adb5826f4354fc9bc90
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-06-12 19:48:49 +0300

    tested

--- __test__/500.test.js
+++ __test__/500.test.js
@@ -5,14 +5,8 @@ const mockRequest = supertest(server);
 
 describe('500 module', () => {
   it('should respond with 500 on invalied route (status)', () => {
-    return mockRequest.get('/api/v1/categories').then((results) => {
+    return mockRequest.put('/api/v1/categories').send({}).then((results) => {
       expect(results.status).toBe(500);
     }).catch(e=>{});
   });
-  it('should respond with 404 on invalied route (statusMessage)', () => {
-    return mockRequest.get('/api/v1/productss/5').then((results) => {
-      expect(results).toMatchObject({ error: 'not Found' });
-    }).catch(e=>{});
-  });
-
 });
